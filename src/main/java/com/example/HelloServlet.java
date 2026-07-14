a19d35494ebbacbbe376fdb234e2922fdc31d260
karthiksiddamallu55-creator <karthiksiddamallu55@gmail.com>
2026-07-14 08:26:54 +0530

    Initial commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<html><body><h1>Hello from the WAR project!</h1></body></html>");
+    }
+}
